9bdfcf9828e3f042b8a3d1ccfe60ffb9897f5c1d
muratcem95 <40249419+muratcem95@users.noreply.github.com>
2019-02-10 14:07:28 -0500

    f

--- views/home/home.js
+++ views/home/home.js
@@ -14,32 +14,13 @@ var onloadCallback = function() {
       };
 
 $("#contactForm").submit(function(e) {
-    e.preventDefault();
-    
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var phone = $("#phone").val();
-    var message = $("#message").val();
-    var recap = $("#recap").val();
-    
     if(!recap) {
+        e.preventDefault();
         var response = grecaptcha.getResponse();
         if(response.length == 0) {
             $("#alert").html('<div class="alert alert-danger text-center alert-dismissible fade show" role="alert"><div id="space"></div><p>Recaptcha is required!</p></div>');
         return false;
         };
-    };
-    
-    if(!name || !email || !phone || !message) {
-        e.preventDefault();
-        $("#alert").html('<div class="alert alert-danger text-center alert-dismissible fade show" role="alert"><div id="space"></div><p>You have to fill all the sections to be able to contact!</p></div>');
-        return false;
-    } else {
-        e.preventDefault();
-        $("#alert").html('<div class="alert alert-primary text-center alert-dismissible fade show" role="alert"><p>Thank you ' + name + ' for contacting!<br>I will get back to you as soon as possible :)</p>');
-        return false;
-        
-        setTimeout(function(){ $(this).unbind('submit').submit() }, 3000);
     };
 }); 
 
